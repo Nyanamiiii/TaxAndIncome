0d1ab6b254efce71b6e561452229710b63929ac6
asday <sara.and.zuka@gmail.com>
2023-03-13 21:55:19 +0000

    Handled invalid input.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 class Income
 {
@@ -6,7 +7,12 @@ class Income
         double tax=0, it;
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter income ");
-        it= sc.nextDouble();
+        try {
+            it= sc.nextDouble();
+        } catch (InputMismatchException exc) {
+            System.out.println("Decimal number expected!");
+            return;
+        }
         if(it<=4579.32)
             tax=0;
         else if(it<=38553.01)
